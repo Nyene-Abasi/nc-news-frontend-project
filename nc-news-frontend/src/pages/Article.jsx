5627ae957356e6a3ca5dc44f492d3aee0fbfd5a8
Nyene-Abasi <nyeneabasi3@gmail.com>
2023-07-20 10:11:57 +0100

    validation included

--- nc-news-frontend/src/pages/Article.jsx
+++ nc-news-frontend/src/pages/Article.jsx
@@ -15,6 +15,7 @@ const Article = () => {
     const [username, setUsername] = useState('')
     const [body, setBody] = useState('')
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false)
     useEffect(() => {
         const fetchArticleAndComments = () => {
           Promise.all([getOneArticle(article_id), getComment(article_id)])
@@ -29,23 +30,31 @@ const Article = () => {
       const handleSubmit = (e) => {
         e.preventDefault();
     
+        if (!username || !body) {
+          alert("Please fill in all the fields before submitting!!!!!");
+          return;
+      }
+
+      setLoading(true)
+  
         const requestedBody = {
           username,
           body,
         };
-        setLoading(true)
+       
         postComment(requestedBody, article_id)
           .then((data) => {
-          
+          alert("Comment posted successfully!");
            setComments((currComments)=>{
             return [data, ...currComments]
            })
            setUsername('')
-           setBody('')
-           
+           setBody('') 
         setLoading(false);
-      }).catch((error) => {
+
+      }).catch(() => {
         setLoading(false); 
+        setError(true)
       
       });
        
@@ -72,7 +81,19 @@ const Article = () => {
 
               
               
-            />     
+            />  
+
+              <h2>Add Comments</h2>
+
+                <form className='form' onSubmit={handleSubmit}>
+                    <label htmlFor="username"></label>
+                    <input type="text" id='username' placeholder='Enter username' value={username} onChange={(e)=>setUsername(e.target.value)}/>
+                    <label htmlFor="comments"></label>
+                    <textarea  id="comments" cols="30" className='textarea' rows="10" placeholder="What's your thought?" value={body} onChange={(e)=>setBody(e.target.value)} ></textarea>
+                    <button type='submit' disabled={loading } className='submit-comment'>Submit</button>
+                    {error ? <p>Failed to post comment. Please try again!</p> : null}
+                </form>  
+
             <div className='comment-parent'>
             <h2 className='commment-header'>Comments</h2>
 
@@ -81,20 +102,9 @@ const Article = () => {
                     key={comment.comment_id}
                     author={comment.author}
                     body={comment.body}
-                    created_at={comment.created_at}
-                    
+                    created_at={comment.created_at}   
                  />
-                ))}  
-                <h2>Add Comments</h2>
-
-                <form className='form' onSubmit={handleSubmit}>
-                    <label htmlFor="username"></label>
-                    <input type="text" id='username' placeholder='Enter username' value={username} onChange={(e)=>setUsername(e.target.value)}/>
-                    <label htmlFor="comments"></label>
-                    <textarea  id="comments" cols="30" className='textarea' rows="10" placeholder="What's your thought?" value={body} onChange={(e)=>setBody(e.target.value)}></textarea>
-                    <button type='submit' disabled={loading} className='submit-comment'>Submit</button>
-                </form>
-                
+                ))}    
             </div>
             </div>
           </>
